0e4dd5609c80db827e6ed658c14d993c82449703
Dilip Singh <dilipsinghoxox@gmail.com>
2024-11-02 10:47:55 +0530

    example of routing and redirecting requests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Import the 'http' module, which provides utilities for creating an HTTP server
+const { log } = require('console');
 const http = require('http');
+const fs = require("fs");
 
 // Create the server using the 'createServer' method, which takes a callback function
 // The callback function is executed each time a request is received
@@ -8,10 +10,38 @@ const server = http.createServer((req, res) => {
     // Log a message to the console whenever the server receives a request
     console.log("Server is running");
 
+    // Log a message to the console 
+    // log(req.url, req.method, req.headers)
+
     // Sending a response back to the client
     // Status code: 200 (OK), Content-Type: text/plain
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n'); // End the response and send the string
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('Hello, World!\n'); // End the response and send the string
+
+    const url = req.url;
+    const method  = req.method;
+    if(url === '/' ){
+        res.write('<html>')
+        res.write('<head> <title>Enter Message</title></head>');
+        res.write('<body> <form action="/message" method="POST"><input type="text"><button type="submit">Click me!</button></form> </body>')
+        res.write('</html>')
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', "Dummy")
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
+
+    res.setHeader("Content-Type", 'text/html');
+    res.write('<html>')
+    res.write('<head> <title>Easy Node</title></head>');
+    res.write('<body> <h1>Easy Node Project</h1> </body>')
+    res.write('</html>')
+    res.end();
 });
 
 // Listen on port 3000 for incoming requests
