80e611ae1b5231590e79dcccfda5ca83f3cb6696
Dilip Singh <dilipsinghoxox@gmail.com>
2024-11-03 18:00:44 +0530

    code updates for expressjs

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require('http'); // Import the 'http' module to create an HTTP server
-const { on } = require('events'); // Import the 'events' module for event handling
-const { log } = require('console'); // Import the 'console' module for logging
-const routes = require('./routes');
-// Create an HTTP server that handles incoming requests
-const server = http.createServer(routes.handler);
-log(routes.someText)
-// Start the server and listen on port 3000
-server.listen(3000, () => {
-    console.log("Server is listening on port 3000"); // Log a message indicating the server is listening
-});
+const express = require('express')
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log("In the middleware2!")
+    res.send('<h1>Hello Frome ExpressJs</h1>')
+})
+
+app.listen(3000);
