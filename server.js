6bd8806f18c7a2ca7e06879e6942c9a15ad1a16a
Dilip Singh <dilipsinghoxox@gmail.com>
2024-11-04 02:14:06 +0530

    using body-parser module

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("In the middleware2!")
+
+// Parsing 
+app.use(bodyParser.urlencoded({extended:false}));
+
+//  Routing
+app.use('/add-customer',(req, res, next)=>{
+    res.send('<form action="/customer" method="POST"> <input type="text" name="title"><button type="submit">add customer</button></form>')
+})
+
+app.use('/customer', (req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req, res, next)=>{
     res.send('<h1>Hello Frome ExpressJs</h1>')
 })
+
 
 app.listen(3000);
