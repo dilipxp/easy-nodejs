d8e740cf4ed111f9428b0f721f966a2704db5378
Dilip Singh <dilipsinghoxox@gmail.com>
2024-11-11 01:12:04 +0530

    adding static css.

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express');
 
+
 const router = express.Router();
+const rootDir = require('../utils/path');
 
 //  Routing
 router.get('/add-customer',(req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-customer.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-customer.html'));
 })
 
 router.post('/customer', (req, res, next)=>{
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./utils/path');
 
-const bodyParser = require('body-parser');
 
 
 // Parsing the incomming request.
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views' ,'error.html'));
+    res.status(404).sendFile(path.join(__dirname,'views' ,'error404.html'));
 });
 
 app.listen(3000);
